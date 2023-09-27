769a1c1b3878f6f9c77173733e02897afada3174
AndrePassaes <andrepassaes@hotmail.com>
2023-09-27 13:47:08 -0300

    Create visitantes forms and views, install widget_tweaks lib and update visitor_registration.html

--- visitantes/views.py
+++ visitantes/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
+from visitantes.forms import VisitorForm
 
 
 def visitor_registration(request):
-    context = {}
+    
+    form = VisitorForm()
+
+    context = {
+        "nome_pagina": "Registrar visitante",
+        "form": form
+    }
 
     return render(request, 'visitor_registration.html', context)
