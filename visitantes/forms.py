730bd0fc46a765f44763f4494bb1e65315ab4d59
AndrePassaes <andrepassaes@hotmail.com>
2023-09-28 22:06:21 -0300

    Create error messages for forms

--- visitantes/forms.py
+++ visitantes/forms.py
@@ -4,7 +4,22 @@ from visitantes.models import Visitor
 class VisitorForm(forms.ModelForm):    
     class Meta:
         model = Visitor
-        fields = {
+        fields = [
             "full_name", "cpf", "birth_date",
-            "house_number", "license_plate", 
+            "house_number", "license_plate" 
+        ]
+        error_messages = {
+            "full_name": {
+                "required": "Nome completo é obrigatório para o registro."
+            },
+            "cpf": {
+                "required": "CPF é obrigatório para o registro."
+            },
+            "birth_date": {
+                "required": "Data de nascimento é obrigatória para o registro.",
+                "invalid": "Por favor, informe um formato válido para data de nascimento (DD/MM/YYYY)."
+            },
+            "house_number": {
+                "required": "Por favor, informe o número da casa a ser visitada."
+            }
         }
